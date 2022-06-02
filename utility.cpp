8c7095462399a8e8636caecd019e774f08f8306a
yosefgoren <yosefisraelgoren@gmail.com>
2022-06-02 22:11:29 +0300

    fixed arg bug

--- utility.cpp
+++ utility.cpp
@@ -11,22 +11,26 @@ void redirectStdin(string input_filename){
 }
 
 int main(int argc, char** argv){
-	string input_filename = "..\\triangle.ds";//later change to first argument if exists.
+	string input_filename = "..\\example.ds";//later change to first argument if exists.
 	if(argc > 1){
 		input_filename = argv[1];
 	}
 	cout << "getting input script from: " << input_filename << endl << endl;
 	redirectStdin(input_filename);
 
+	bool printlib = true;
 	for(int i = 0; i < argc; ++i)
-		if(argv[i] != "nolib"){
-			cout << "L_{0}\\left(x,y,t\\right)=\\frac{t-x}{y-x}" << endl;
-			cout << "L_{1}\\left(x,y,t\\right)=\\left(1-t\\right)x+ty" << endl;
-			cout << "L_{split}(t,k)=L_{0}(0,\\frac{1}{k},\\operatorname{mod}(t,\\frac{1}{k}))" << endl;
-			cout << "L_{index}(t,k)=\\operatorname{floor}(kt)+1" << endl;
-			cout << endl;
-		}
-	
+		if(argv[i] == "nolib")
+			printlib = false;
+
+	if(printlib){
+		cout << "L_{0}\\left(x,y,t\\right)=\\frac{t-x}{y-x}" << endl;
+		cout << "L_{1}\\left(x,y,t\\right)=\\left(1-t\\right)x+ty" << endl;
+		cout << "L_{split}(t,k)=L_{0}(0,\\frac{1}{k},\\operatorname{mod}(t,\\frac{1}{k}))" << endl;
+		cout << "L_{index}(t,k)=\\operatorname{floor}(kt)+1" << endl;
+		cout << endl;
+	}
+
 	try{
 		Symtab symtab = Symtab();
 		#if(YYDEBUG == 1)
@@ -36,7 +40,7 @@ int main(int argc, char** argv){
 	} catch(const SemanticError& e){
 		// cout << "\nline " << yylineno << ": " << e.errorMsg() << endl; //TODO: enable this!
 	}
-	cout << "0" << endl;
+	cout << endl << "0" << endl;
 	return 0;
 }
 
